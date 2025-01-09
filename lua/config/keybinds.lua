690b97a8f3dc6a6e65e69d788ed22bb9b818d81e
Sebastian Hempel Linde <sebastian@hempellinde.com>
2025-01-09 07:39:58 +0100

    update

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,9 +1,23 @@
 -- Keybindings
 
+-- [[ Utilities ]]
+local nmap_leader = function(suffix, rhs, desc, opts)
+  opts = opts or {}
+  opts.desc = desc
+  vim.keymap.set('n', '<leader>' .. suffix, rhs, opts)
+end
+
+-- [[ Defaults ]]
+vim.keymap.set('n', '<C-æ>', '<C-]>')
+
 -- [[ Explorer ]]
-vim.keymap.set('n', '<leader>e', '<CMD>15 Lexplore<CR>', { desc = 'File Explorer' })
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+nmap_leader('e', '<cmd>Oil<cr>', 'Open Parent Directory')
+vim.keymap.set("n", "-", "<cmd>lua require('oil').open_float('.')<cr>", { desc = "Open parent directory" })
 
 -- [[ Finder ]]
 vim.keymap.set('n', '<leader>ff', '<CMD>Telescope find_files<CR>', { desc = 'Find Files' })
 vim.keymap.set('n', '<leader>fh', '<CMD>Telescope help_tags<CR>', { desc = 'Find Files' })
+
+-- [[ Overseer ]]
+nmap_leader('oo', '<cmd>OverseerRun<cr>', 'Overseer Run')
+
--- lua/plugin/oil.lua
+++ lua/plugin/oil.lua
@@ -1,7 +1,42 @@
-local M = { 'stevearc/oil.nvim' }
-
-M.event = 'VeryLazy'
-
-M.opts = {}
-
-return M
+return { 
+  'stevearc/oil.nvim',
+  event = 'VeryLazy',
+  opts = {
+    delete_to_trash = true,
+    skip_confirm_for_simple_edits = true,
+    watch_for_changes = false,
+    keymaps = {
+      ["g?"] = { "actions.show_help", mode = "n" },
+      ["<CR>"] = "actions.select",
+      ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+      ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+      ["<C-t>"] = { "actions.select", opts = { tab = true } },
+      ["<C-p>"] = "actions.preview",
+      ["<C-c>"] = { "actions.close", mode = "n" },
+      ["q"] = { "actions.close", mode = "n" },
+      ["<C-l>"] = "actions.refresh",
+      ["-"] = { "actions.parent", mode = "n" },
+      ["_"] = { "actions.open_cwd", mode = "n" },
+      ["`"] = { "actions.cd", mode = "n" },
+      ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+      ["gs"] = { "actions.change_sort", mode = "n" },
+      ["gx"] = "actions.open_external",
+      ["g."] = { "actions.toggle_hidden", mode = "n" },
+      ["g\\"] = { "actions.toggle_trash", mode = "n" },
+    },
+    float = {
+      padding = 2,
+      max_width = 0.62,
+      max_height = 0.62,
+      border = "rounded",
+      win_options = {
+        winblend = 25,
+      },
+    },
+    preview_win = {
+      win_options = {
+        winblend = 25
+      }
+    },
+  }
+}
