75b8610fb16078b9d5f46d61412fcc834bbdf4d3
Sebastian Hempel Linde <stl@yco.dk>
2025-01-10 09:33:41 +0100

    feat: add org-mode and vertical terminal

--- lua/plugin/snacks.lua
+++ lua/plugin/snacks.lua
@@ -42,7 +42,7 @@ return {
     { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
     { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
     { "<leader>tt", function() Snacks.terminal() end, desc = "Toggle Terminal" },
-    { "<leader>tv", function() Snacks.terminal() end, desc = "Toggle Terminal" },
+    { "<leader>tv", function() Snacks.terminal({"fish"}, { win = { position = "right" }}) end, desc = "Toggle Terminal" },
     { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
     { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
     { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
